f9a3b6098a42f73f6967f1a5daf8a009b15c7de3
ZeynepEzgiYakisan <115227006+ZeynepEzgiYakisan@users.noreply.github.com>
2023-01-16 19:36:52 +0300

    Add files via upload

---
+++ cafesiparisuygulamasi/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿
+namespace cafesiparisuygulamasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnCayArti = new System.Windows.Forms.Button();
+            this.btnCayEksi = new System.Windows.Forms.Button();
+            this.btnKahveArti = new System.Windows.Forms.Button();
+            this.btnKolaArti = new System.Windows.Forms.Button();
+            this.btnSuArti = new System.Windows.Forms.Button();
+            this.btnSuEksi = new System.Windows.Forms.Button();
+            this.btnKolaEksi = new System.Windows.Forms.Button();
+            this.btnKahveEksi = new System.Windows.Forms.Button();
+            this.btnTemizle = new System.Windows.Forms.Button();
+            this.btnSiparis = new System.Windows.Forms.Button();
+            this.lblCay = new System.Windows.Forms.Label();
+            this.lblKahve = new System.Windows.Forms.Label();
+            this.lblKola = new System.Windows.Forms.Label();
+            this.lblSu = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Sylfaen", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(67, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ÇAY";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Sylfaen", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(56, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "KAHVE";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Sylfaen", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(61, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 29);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "KOLA";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Sylfaen", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(71, 196);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 29);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "SU";
+            // 
+            // btnCayArti
+            // 
+            this.btnCayArti.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnCayArti.Location = new System.Drawing.Point(375, 45);
+            this.btnCayArti.Name = "btnCayArti";
+            this.btnCayArti.Size = new System.Drawing.Size(35, 29);
+            this.btnCayArti.TabIndex = 4;
+            this.btnCayArti.Text = "+";
+            this.btnCayArti.UseVisualStyleBackColor = false;
+            this.btnCayArti.Click += new System.EventHandler(this.btnCayArti_Click);
+            // 
+            // btnCayEksi
+            // 
+            this.btnCayEksi.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnCayEksi.Location = new System.Drawing.Point(483, 45);
+            this.btnCayEksi.Name = "btnCayEksi";
+            this.btnCayEksi.Size = new System.Drawing.Size(35, 29);
+            this.btnCayEksi.TabIndex = 5;
+            this.btnCayEksi.Text = "-";
+            this.btnCayEksi.UseVisualStyleBackColor = false;
+            this.btnCayEksi.Click += new System.EventHandler(this.btnCayEksi_Click);
+            // 
+            // btnKahveArti
+            // 
+            this.btnKahveArti.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnKahveArti.Location = new System.Drawing.Point(375, 96);
+            this.btnKahveArti.Name = "btnKahveArti";
+            this.btnKahveArti.Size = new System.Drawing.Size(35, 29);
+            this.btnKahveArti.TabIndex = 6;
+            this.btnKahveArti.Text = "+";
+            this.btnKahveArti.UseVisualStyleBackColor = false;
+            this.btnKahveArti.Click += new System.EventHandler(this.btnKahveArti_Click);
+            // 
+            // btnKolaArti
+            // 
+            this.btnKolaArti.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnKolaArti.Location = new System.Drawing.Point(375, 147);
+            this.btnKolaArti.Name = "btnKolaArti";
+            this.btnKolaArti.Size = new System.Drawing.Size(35, 29);
+            this.btnKolaArti.TabIndex = 7;
+            this.btnKolaArti.Text = "+";
+            this.btnKolaArti.UseVisualStyleBackColor = false;
+            this.btnKolaArti.Click += new System.EventHandler(this.btnKolaArti_Click);
+            // 
+            // btnSuArti
+            // 
+            this.btnSuArti.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSuArti.Location = new System.Drawing.Point(375, 198);
+            this.btnSuArti.Name = "btnSuArti";
+            this.btnSuArti.Size = new System.Drawing.Size(35, 29);
+            this.btnSuArti.TabIndex = 8;
+            this.btnSuArti.Text = "+";
+            this.btnSuArti.UseVisualStyleBackColor = false;
+            this.btnSuArti.Click += new System.EventHandler(this.btnSuArti_Click);
+            // 
+            // btnSuEksi
+            // 
+            this.btnSuEksi.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSuEksi.Location = new System.Drawing.Point(483, 198);
+            this.btnSuEksi.Name = "btnSuEksi";
+            this.btnSuEksi.Size = new System.Drawing.Size(35, 29);
+            this.btnSuEksi.TabIndex = 9;
+            this.btnSuEksi.Text = "-";
+            this.btnSuEksi.UseVisualStyleBackColor = false;
+            this.btnSuEksi.Click += new System.EventHandler(this.btnSuEksi_Click);
+            // 
+            // btnKolaEksi
+            // 
+            this.btnKolaEksi.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnKolaEksi.Location = new System.Drawing.Point(483, 147);
+            this.btnKolaEksi.Name = "btnKolaEksi";
+            this.btnKolaEksi.Size = new System.Drawing.Size(35, 29);
+            this.btnKolaEksi.TabIndex = 10;
+            this.btnKolaEksi.Text = "-";
+            this.btnKolaEksi.UseVisualStyleBackColor = false;
+            this.btnKolaEksi.Click += new System.EventHandler(this.btnKolaEksi_Click);
+            // 
+            // btnKahveEksi
+            // 
+            this.btnKahveEksi.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnKahveEksi.Location = new System.Drawing.Point(483, 96);
+            this.btnKahveEksi.Name = "btnKahveEksi";
+            this.btnKahveEksi.Size = new System.Drawing.Size(35, 29);
+            this.btnKahveEksi.TabIndex = 11;
+            this.btnKahveEksi.Text = "-";
+            this.btnKahveEksi.UseVisualStyleBackColor = false;
+            this.btnKahveEksi.Click += new System.EventHandler(this.btnKahveEksi_Click);
+            // 
+            // btnTemizle
+            // 
+            this.btnTemizle.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnTemizle.Location = new System.Drawing.Point(134, 292);
+            this.btnTemizle.Name = "btnTemizle";
+            this.btnTemizle.Size = new System.Drawing.Size(118, 29);
+            this.btnTemizle.TabIndex = 12;
+            this.btnTemizle.Text = "TEMİZLE ";
+            this.btnTemizle.UseVisualStyleBackColor = false;
+            this.btnTemizle.Click += new System.EventHandler(this.btnTemizle_Click);
+            // 
+            // btnSiparis
+            // 
+            this.btnSiparis.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSiparis.Location = new System.Drawing.Point(341, 292);
+            this.btnSiparis.Name = "btnSiparis";
+            this.btnSiparis.Size = new System.Drawing.Size(140, 29);
+            this.btnSiparis.TabIndex = 13;
+            this.btnSiparis.Text = "SİPARİŞ VER";
+            this.btnSiparis.UseVisualStyleBackColor = false;
+            this.btnSiparis.Click += new System.EventHandler(this.btnSiparis_Click);
+            // 
+            // lblCay
+            // 
+            this.lblCay.AutoSize = true;
+            this.lblCay.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblCay.Location = new System.Drawing.Point(234, 43);
+            this.lblCay.Name = "lblCay";
+            this.lblCay.Size = new System.Drawing.Size(18, 20);
+            this.lblCay.TabIndex = 14;
+            this.lblCay.Text = "0";
+            // 
+            // lblKahve
+            // 
+            this.lblKahve.AutoSize = true;
+            this.lblKahve.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblKahve.Location = new System.Drawing.Point(234, 96);
+            this.lblKahve.Name = "lblKahve";
+            this.lblKahve.Size = new System.Drawing.Size(18, 20);
+            this.lblKahve.TabIndex = 15;
+            this.lblKahve.Text = "0";
+            // 
+            // lblKola
+            // 
+            this.lblKola.AutoSize = true;
+            this.lblKola.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblKola.Location = new System.Drawing.Point(234, 149);
+            this.lblKola.Name = "lblKola";
+            this.lblKola.Size = new System.Drawing.Size(18, 20);
+            this.lblKola.TabIndex = 16;
+            this.lblKola.Text = "0";
+            // 
+            // lblSu
+            // 
+            this.lblSu.AutoSize = true;
+            this.lblSu.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblSu.Location = new System.Drawing.Point(234, 202);
+            this.lblSu.Name = "lblSu";
+            this.lblSu.Size = new System.Drawing.Size(18, 20);
+            this.lblSu.TabIndex = 17;
+            this.lblSu.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblSu);
+            this.Controls.Add(this.lblKola);
+            this.Controls.Add(this.lblKahve);
+            this.Controls.Add(this.lblCay);
+            this.Controls.Add(this.btnSiparis);
+            this.Controls.Add(this.btnTemizle);
+            this.Controls.Add(this.btnKahveEksi);
+            this.Controls.Add(this.btnKolaEksi);
+            this.Controls.Add(this.btnSuEksi);
+            this.Controls.Add(this.btnSuArti);
+            this.Controls.Add(this.btnKolaArti);
+            this.Controls.Add(this.btnKahveArti);
+            this.Controls.Add(this.btnCayEksi);
+            this.Controls.Add(this.btnCayArti);
+            this.Name = "Form1";
+            this.Text = "Kafe Sipariş";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnCayArti;
+        private System.Windows.Forms.Button btnCayEksi;
+        private System.Windows.Forms.Button btnKahveArti;
+        private System.Windows.Forms.Button btnKolaArti;
+        private System.Windows.Forms.Button btnSuArti;
+        private System.Windows.Forms.Button btnSuEksi;
+        private System.Windows.Forms.Button btnKolaEksi;
+        private System.Windows.Forms.Button btnKahveEksi;
+        private System.Windows.Forms.Button btnTemizle;
+        private System.Windows.Forms.Button btnSiparis;
+        private System.Windows.Forms.Label lblCay;
+        private System.Windows.Forms.Label lblKahve;
+        private System.Windows.Forms.Label lblKola;
+        private System.Windows.Forms.Label lblSu;
+    }
+}
+
