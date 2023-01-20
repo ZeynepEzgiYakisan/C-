be8462a19c8ce2e71759a089dd4e1310a354625e
ZeynepEzgiYakisan <115227006+ZeynepEzgiYakisan@users.noreply.github.com>
2023-01-20 12:07:32 +0300

    Add files via upload

---
+++ cafesiparis-2/cafesiparis/Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿
+namespace odev3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(431, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 37);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "KAHVECİM";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Coral;
+            this.label2.Location = new System.Drawing.Point(196, 123);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label2.Size = new System.Drawing.Size(124, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "SICAK KAHVELER";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.ForeColor = System.Drawing.Color.Coral;
+            this.label9.Location = new System.Drawing.Point(431, 123);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(132, 20);
+            this.label9.TabIndex = 6;
+            this.label9.Text = "SOĞUK KAHVELER";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.ForeColor = System.Drawing.Color.Coral;
+            this.label13.Location = new System.Drawing.Point(734, 123);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(124, 20);
+            this.label13.TabIndex = 10;
+            this.label13.Text = "TÜRK KAHVELERİ";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Items.AddRange(new object[] {
+            "\t",
+            "Espresso",
+            "Americano",
+            "Latte"});
+            this.listBox1.Location = new System.Drawing.Point(167, 160);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(188, 104);
+            this.listBox1.TabIndex = 14;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Items.AddRange(new object[] {
+            "",
+            "İced Latte",
+            "İced Macchiato",
+            "İced Americano"});
+            this.listBox2.Location = new System.Drawing.Point(400, 160);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(206, 104);
+            this.listBox2.TabIndex = 15;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 20;
+            this.listBox3.Items.AddRange(new object[] {
+            "",
+            "Damla Sakızlı Türk Kahvesi",
+            "Menengiç Türk Kahvesi",
+            "Özel Türk Kahvesi"});
+            this.listBox3.Location = new System.Drawing.Point(669, 160);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(279, 104);
+            this.listBox3.TabIndex = 16;
+            this.listBox3.SelectedIndexChanged += new System.EventHandler(this.listBox3_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 286);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 20);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "FİYAT";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(259, 286);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 20);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(495, 286);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(17, 20);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(815, 286);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(17, 20);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(400, 372);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(206, 29);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "SEPETE EKLE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.ItemHeight = 20;
+            this.listBox4.Location = new System.Drawing.Point(50, 457);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(488, 124);
+            this.listBox4.TabIndex = 22;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(31, 415);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(70, 20);
+            this.label7.TabIndex = 23;
+            this.label7.Text = "Sepetiniz";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(31, 603);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(94, 20);
+            this.label8.TabIndex = 24;
+            this.label8.Text = "Toplam Fiyat";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(126, 596);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 27);
+            this.textBox1.TabIndex = 25;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(625, 457);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(93, 24);
+            this.checkBox1.TabIndex = 26;
+            this.checkBox1.Text = "ÖĞRENCİ";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(625, 487);
+            this.button2.Name = "button2";
+            this.button2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button2.Size = new System.Drawing.Size(207, 94);
+            this.button2.TabIndex = 27;
+            this.button2.Text = "FATURA OLUŞTUR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(867, 461);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(381, 162);
+            this.richTextBox1.TabIndex = 28;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(1224, 731);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ cafesiparis-2/cafesiparis/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace odev3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.Text =="")
+            {
+                label4.Text = "0";
+            }
+            else if (listBox1.Text == "Espresso")
+            {
+                label4.Text = "30";
+            }
+            else if (listBox1.Text == "Americano")
+            {
+                label4.Text = "40";
+            }
+            else if (listBox1.Text == "Latte")
+            {
+                label4.Text = "45";
+            }
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox2.Text == "")
+            {
+                label5.Text = "0";
+            }
+            else if (listBox2.Text == "İced Latte")
+            {
+                label5.Text = "40";
+            }
+            else if (listBox2.Text == "İced Macchiato")
+            {
+                label5.Text = "50";
+            }
+            else if (listBox2.Text == "İced Americano")
+            {
+                label5.Text = "45";
+            }
+        }
+
+        private void listBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox3.Text == "")
+            {
+                label6.Text = "0";
+            }
+            else if (listBox3.Text == "Damla Sakızlı Türk Kahvesi")
+            {
+                label6.Text = "30";
+            }
+            else if (listBox3.Text == "Menengiç Türk Kahvesi")
+            {
+                label6.Text = "50";
+            }
+            else if (listBox3.Text == "Özel Türk Kahvesi")
+            {
+                label6.Text = "45";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int toplam;
+            listBox4.Items.Clear();
+
+            if (listBox1.Text != "")
+            {
+                listBox4.Items.Add("SICAK KAHVELER : " + " "+ listBox1.Text + "  "+ label4.Text);
+            }
+
+            if (listBox2.Text != "")
+            {
+                listBox4.Items.Add("SOĞUK KAHVELER : " +" "+ listBox2.Text + "  "+ label5.Text);
+            }
+
+            if (listBox3.Text != "")
+            {
+                listBox4.Items.Add("TÜRK KAHVELERİ : " + " " + listBox3.Text + "  " + label6.Text);
+            }
+
+            toplam = int.Parse(label4.Text) + int.Parse(label5.Text) + int.Parse(label6.Text);
+            textBox1.Text = toplam.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double toplam, ogrenci, indirim,odenecekTutar;
+            toplam = double.Parse(textBox1.Text);
+
+            if(checkBox1.Checked)
+            {
+                ogrenci = toplam / 20;
+            }
+            else
+            {
+                ogrenci = 0;
+            }
+            if(toplam >= 100)
+            {
+                indirim = toplam / 10;
+            }
+            else
+            {
+                indirim = 0;
+            }
+            odenecekTutar = toplam - indirim - ogrenci;
+            richTextBox1.Text = "";
+            richTextBox1.Text += "TOPLAM TUTAR = " + toplam + "\n";
+            richTextBox1.Text += "İNDİRİM TUTARI = " + indirim + "\n";
+            richTextBox1.Text += "ÖĞRENCİ İNDİRİMİ = " + ogrenci + "\n";
+            richTextBox1.Text += "-----------------------------------------";
+            richTextBox1.Text += "ÖDENECEK TUTAR = " + odenecekTutar + "\n";
+
+
+
+        }
+    }
+}
